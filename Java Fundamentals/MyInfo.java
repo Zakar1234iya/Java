86cbcd08d839f71624f7f1161a6a91f0c9fc3d24
Zakaria Jibreen <zakari1234a@gmail.com>
2024-11-26 10:57:23 +0200

    first project

---
+++ Java Fundamentals/MyInfo.java
@@ -0,0 +1,7 @@
+public class MyInfo {
+    public static void main(String[] args) {
+        System.out.println("My name is Zakaria Jibreen");
+        System.out.println("I am 25 years old");
+        System.out.println("My hometown is Ramallah, Palestine");
+    }
+}
