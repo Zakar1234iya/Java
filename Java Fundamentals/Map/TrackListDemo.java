8fac2cad7a3ccad5338f378a6be1f422a599b48a
Zakaria Jibreen <zakari1234a@gmail.com>
2024-12-08 18:43:13 +0200

    bigsam song

--- Java Fundamentals/Map/TrackListDemo.java
+++ Java Fundamentals/Map/TrackListDemo.java
@@ -3,22 +3,22 @@ import java.util.Map;
 
 public class TrackListDemo {
     public static void main(String[] args) {
-        // Create a HashMap to store track titles and lyrics
+        // Create a HashMap 
         HashMap<String, String> trackList = new HashMap<>();
 
-        // Add songs to the trackList
-        trackList.put("Uprising", "Paranoia is in bloom, The PR transmissions will resume...");
-        trackList.put("Starlight", "Far away, this ship is taking me far away...");
-        trackList.put("Supermassive Black Hole", "Oh baby, don't you know I suffer? Oh baby, can you hear me moan?");
-        trackList.put("Knights of Cydonia", "Come ride with me through the veins of history...");
+        // Add songs
+        trackList.put("Haifa", "If only once, If only once, You fell asleep in my tired soul...");
+        trackList.put("Qooli", "Tell me, tell me, tell me, tell me, tell me, tell me...");
+        trackList.put("Drama", "Drama, drama, drama, drama, drama, drama...");
+        trackList.put("Ma Bithoon", "You won't be forgotten, You won't be forgotten...");
 
-        // print songs
-        String songTitle = "Starlight";
+        //pring song
+        String songTitle = "Qooli";
         String lyrics = trackList.get(songTitle);
         System.out.println("Lyrics for " + songTitle + ": " + lyrics);
 
         // Print all 
-        System.out.println("Track List:");
+        System.out.println("All Tracks:");
         for (Map.Entry<String, String> entry : trackList.entrySet()) {
             System.out.println("Track: " + entry.getKey() + " - Lyrics: " + entry.getValue());
         }
